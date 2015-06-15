49c21ef579dace491f56418a80885b4825ea8d75
babumuralidharan <babumuralidharan@users.noreply.github.com>
2015-06-15 15:34:02 -0700

    Update OuterLoopTestsDiscoverer.cs

--- src/Common/tests/XunitTraitsDiscoverers/OuterLoopTestsDiscoverer.cs
+++ src/Common/tests/XunitTraitsDiscoverers/OuterLoopTestsDiscoverer.cs
@@ -23,4 +23,16 @@ namespace Xunit.TraitDiscoverers
             yield return new KeyValuePair<string, string>("category", "outerloop");
         }
     }
+    public class OuterLoopTestsDiscoverer : ITraitDiscoverer
+    {
+        /// <summary>
+        /// Gets the trait values from the Category attribute.
+        /// </summary>
+        /// <param name="traitAttribute">The trait attribute containing the trait values.</param>
+        /// <returns>The trait values.</returns>
+        public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
+        {
+            yield return new KeyValuePair<string, string>("category", "outerloop");
+        }
+    }
 }
